c54dfc220eecda476ddbb5f93640ed91fc0ff584
OsnatProgrammer <116493968+OsnatProgrammer@users.noreply.github.com>
2023-06-26 14:24:24 +0300

    summary

---
+++ src/components/general_comps/summary.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Summary() {
+    return (
+        <div className='container'>
+            Welcome to RouteRide - Your On-Demand Ride Matching Platform!
+            At RouteRide, we've revolutionized the way people travel by creating a convenient and safe platform that connects drivers and hitchhikers. Whether you're a driver looking to share your journey or a hitchhiker seeking a ride, our location-based services and user preferences will ensure you have a seamless and enjoyable travel experience.
+            For drivers willing to offer rides along their route, simply enter your departure and destination addresses into our system. RouteRide will then match you with hitchhikers who are on your route or within a certain distance, allowing you to share your trip and make the most of your empty seats. It's an excellent way to meet new people, save money, and contribute to a more sustainable mode of transportation.
+            Hitchhikers, on the other hand, can join RouteRide by entering their departure address and estimated departure time. Once you're registered, our platform will notify you when a suitable trip becomes available. No more waiting by the side of the road; with RouteRide, you can find rides that align with your travel plans effortlessly.
+            Once a ride match is made, both the driver and hitchhiker can communicate with each other through our secure chat feature. This ensures effective coordination and allows you to discuss any additional details or preferences before embarking on the journey.
+            At RouteRide, we prioritize safety and user satisfaction. Our site administrator actively monitors user complaints and requests, taking necessary actions
+        </div>
+    )
+}
