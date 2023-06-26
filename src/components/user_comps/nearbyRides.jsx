74d9631d46b446f8a2245cd2c442380453384f7a
OsnatProgrammer <116493968+OsnatProgrammer@users.noreply.github.com>
2023-06-26 21:51:15 +0300

    nearbyRides

--- src/components/user_comps/nearbyRides.jsx
+++ src/components/user_comps/nearbyRides.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { doApiGet, API_URL, TOKEN_NAME, CURRENT_USER } from '../services/apiService';
+import RideOfferItem from './rideOfferItem';
+import RideRequestItem from './rideRequestItem';
+
+
+export const getAllridesRequestsOpen = async () => {
+  let url = API_URL + "/rideRequests/getAllridesRequestsOpen";
+  try {
+    const response = await doApiGet(url);
+    console.log("Requests", response);
+    const ridesRequests = response.data.ar_rideRequests;
+    console.log(ridesRequests);
+    return ridesRequests;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch ridesRequests");
+  }
+};
+
+export const getAllridesoffersOpen = async () => {
+  let url = API_URL + "/rideoffers/getAllridesoffersOpen";
+  try {
+    const response = await doApiGet(url);
+    console.log("Offer", response);
+    const ridesoffers = response.data.ar_rideoffers;
+    console.log(ridesoffers);
+    return ridesoffers;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch ridesoffers");
+  }
+};
+
 
 export default function NearbyRides() {
+  const [ridesRequests, setRidesRequests] = useState([]);
+  const [ridesoffers, setridesoffers] = useState([]);
+
+  useEffect(() => {
+    async function fetchrides() {
+      try {
+        const ridesRequestsData = await getAllridesRequestsOpen();
+        setRidesRequests(ridesRequestsData);
+        const ridesoffersData = await getAllridesoffersOpen();
+        setridesoffers(ridesoffersData);
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchrides();
+  }, []);
+
   return (
-    <div>NearbyRides</div>
+    <div className='container'>
+      <h2>Rides Reques: </h2>
+      <div className='row g-3'>
+        {ridesRequests.map(item => {
+          return (
+            <RideRequestItem key={item.id} item={item} />
+            )
+          })}
+        {ridesRequests.length < 1 && <h2>Loading...</h2>}
+      </div>
+      <div className='row g-3'>
+        <h2>Rides Offer: </h2>
+        {ridesoffers.map(item => {
+          return (
+            <RideOfferItem key={item.id} item={item} />
+          )
+        })}
+        {ridesoffers.length < 1 && <h2>Loading...</h2>}
+      </div>
+    </div>
   )
 }
--- src/components/user_comps/ridesHistory.jsx
+++ src/components/user_comps/ridesHistory.jsx
@@ -2,10 +2,14 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { doApiGet, API_URL, TOKEN_NAME, CURRENT_USER } from '../services/apiService';
 
-import $ from 'jquery';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'datatables.net-dt/css/jquery.dataTables.css';
-import 'datatables.net';
+// import $ from 'jquery';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'datatables.net-dt/css/jquery.dataTables.css';
+// import 'datatables.net';
+// import 'datatables.net-bs4';
+// import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css'; 
+
+
 export const getAllRides = async () => {
     try {
         const url = API_URL + "/rides/getAllRidesRequest";
@@ -37,48 +41,66 @@ export default function RidesHistory() {
         fetchRides();
     }, []);
 
-    useEffect(() => {
-        $(document).ready(function () {
-            $('#dtBasicExample').DataTable();
-            $('.dataTables_length').addClass('bs-select');
-        });
-    }, []);
+    // useEffect(() => {
+    //     // Initialize the DataTable on component mount
+    //     $(document).ready(function () {
+    //       $('#dtBasicExample').DataTable({
+    //         // Enable column filtering
+    //         initComplete: function () {
+    //           this.api().columns().every(function () {
+    //             var column = this;
+    //             var select = $('<select><option value=""></option></select>')
+    //               .appendTo($(column.header()))
+    //               .on('change', function () {
+    //                 var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+    //                 column
+    //                   .search(val ? '^' + val + '$' : '', true, false)
+    //                   .draw();
+    //               });
+
+    //             column.data().unique().sort().each(function (d, j) {
+    //               select.append('<option value="' + d + '">' + d + '</option>');
+    //             });
+    //           });
+    //         }
+    //       });
+    //     });
+    //   }, []);
 
     return (
         <div className="container">
             <h1>Rides History</h1>
-            {rides.length > 0 ? (
-                <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
-                    <thead className="thead-dark">
-                        <tr>
-                            <th>Ride date</th>
-                            <th>Ride time</th>
-                            <th>Departure address</th>
-                            <th>Destination address</th>
-                            <th>Status</th>
+            <table id="dtBasicExample" className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%">
+            {/* <table className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%"> */}
+                <thead className="thead-dark">
+                    <tr>
+                        <th>Ride date</th>
+                        <th>Ride time</th>
+                        <th>Departure address</th>
+                        <th>Destination address</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rides.map((ride) => (
+                        <tr key={ride.rideID}>
+                            <td>{new Date(ride.details_Request.departure_time).toLocaleDateString()}</td>
+                            <td>{new Date(ride.details_Request.departure_time).toLocaleTimeString()}</td>
+                            <td>{ride.details_Request.departure_address}</td>
+                            <td>{ride.details_Request.destination_address}</td>
+                            <td>{ride.details_Request.status}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {rides.map((ride) => (
-                            <tr key={ride.rideID}>
-                                <td>{new Date(ride.details_Request.departure_time).toLocaleDateString()}</td>
-                                <td>{new Date(ride.details_Request.departure_time).toLocaleTimeString()}</td>
-                                <td>{ride.details_Request.departure_address}</td>
-                                <td>{ride.details_Request.destination_address}</td>
-                                <td>{ride.details_Request.status}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            ) : (<>
-                <div class="spinner-border text-warning" role="status">
-                </div>
-                    {/* <span class="sr-only">Loading...</span> */}
-                <p>No rides available</p></>
-            )}
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
+
+
+
+
 
 
 
