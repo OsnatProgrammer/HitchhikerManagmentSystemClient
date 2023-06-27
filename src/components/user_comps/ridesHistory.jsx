469149122c29bc9353a53e720207c65b276135d4
OsnatProgrammer <116493968+OsnatProgrammer@users.noreply.github.com>
2023-06-27 10:56:35 +0300

    ridesHistory

--- src/components/user_comps/ridesHistory.jsx
+++ src/components/user_comps/ridesHistory.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { doApiGet, API_URL, TOKEN_NAME, CURRENT_USER } from '../services/apiService';
+import {GoogleMap} from 'react-google-maps';
 
 // import $ from 'jquery';
 // import 'bootstrap/dist/css/bootstrap.css';
@@ -10,13 +11,13 @@ import { doApiGet, API_URL, TOKEN_NAME, CURRENT_USER } from '../services/apiServ
 // import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css'; 
 
 
-export const getAllRides = async () => {
+export const getAllRidesById = async () => {
     try {
-        const url = API_URL + "/rides/getAllRidesRequest";
+        const url = API_URL + "/rides/getAllRidesById";
         const response = await doApiGet(url);
 
-        const rides = response.data.arr;
-        console.log(rides);
+        const rides = [];
+        rides.push(response.data.rides)
         return rides;
     } catch (err) {
         console.log(err);
@@ -31,8 +32,9 @@ export default function RidesHistory() {
     useEffect(() => {
         async function fetchRides() {
             try {
-                const ridesData = await getAllRides();
+                const ridesData = await getAllRidesById();
                 setRides(ridesData);
+                console.log(ridesData);
             } catch (err) {
                 console.log(err);
             }
@@ -68,34 +70,39 @@ export default function RidesHistory() {
     //     });
     //   }, []);
 
-    return (
-        <div className="container">
-            <h1>Rides History</h1>
-            <table id="dtBasicExample" className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%">
-            {/* <table className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%"> */}
-                <thead className="thead-dark">
-                    <tr>
-                        <th>Ride date</th>
-                        <th>Ride time</th>
-                        <th>Departure address</th>
-                        <th>Destination address</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rides.map((ride) => (
-                        <tr key={ride.rideID}>
-                            <td>{new Date(ride.details_Request.departure_time).toLocaleDateString()}</td>
-                            <td>{new Date(ride.details_Request.departure_time).toLocaleTimeString()}</td>
-                            <td>{ride.details_Request.departure_address}</td>
-                            <td>{ride.details_Request.destination_address}</td>
-                            <td>{ride.details_Request.status}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+    // return (
+    //     <div className="container">
+    //         <h1>Rides History</h1>
+    //         {rides.length > 0 ? (
+    //             <table id="dtBasicExample" className="table table-striped table-bordered table-sm" cellSpacing="0" width="100%">
+    //                 <thead className="thead-dark">
+    //                     <tr>
+    //                         <th>Ride date</th>
+    //                         <th>Ride time</th>
+    //                         <th>Departure address</th>
+    //                         <th>Destination address</th>
+    //                         <th>Status</th>
+    //                     </tr>
+    //                 </thead>
+    //                 <tbody>
+    //                     {rides.map((ride) => (
+    //                         <tr key={ride.rideID}>
+    //                             <td>{new Date(ride.details_Request.departure_time).toLocaleDateString()}</td>
+    //                             <td>{new Date(ride.details_Request.departure_time).toLocaleTimeString()}</td>
+    //                             <td>{ride.details_Request.departure_address}</td>
+    //                             <td>{ride.details_Request.destination_address}</td>
+    //                             <td>{ride.details_Request.status}</td>
+    //                         </tr>
+    //                     ))}
+    //                 </tbody>
+    //             </table>
+    //         ) : (
+    //             <p>No rides available</p>
+    //         )}
+    //     </div>
+{/* <GoogleMap apiKey={apiKey} /> */}
+
+    // );
 }
 
 
