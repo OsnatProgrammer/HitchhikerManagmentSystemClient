54ac2bed931810a22d7a7bd0a10e3076fbe63e73
HadassahAckerman <116038042+HadassahAckerman@users.noreply.github.com>
2023-07-04 15:47:30 +0300

    editMyInfo

--- src/components/general_comps/myInfo.jsx
+++ src/components/general_comps/myInfo.jsx
@@ -11,8 +11,12 @@ import InputEmail from '../inputs/inputEmail';
 import SelectGender from '../inputs/selectGender';
 import AddressInput from '../user_comps/addressInput';
 import InputPassword from '../inputs/inputPassword';
+import { bottom } from '@popperjs/core';
 
 const user = JSON.parse(localStorage.getItem(CURRENT_USER));
+const imageFemale = "https://media.istockphoto.com/id/1098017436/photo/portrait-of-a-young-woman-side-view.jpg?s=612x612&w=0&k=20&c=Tr3GW0WT5ytzuj6JcLV98R4MDTtB6i2K3Z8gvqPTCpA=";
+const imageMale = "https://media.istockphoto.com/id/498085583/photo/silhouette-of-the-man-on-a-white-background.jpg?b=1&s=612x612&w=0&k=20&c=qtvfp762Uyes36ThewDwdRnTuj1xf45rHJEDsk4ius0=";
+
 
 export const updateUser = async (userId, updatedUserData) => {
   try {
@@ -76,8 +80,8 @@ export default function MyInfo() {
     try {
       const updatedUser = await updateUser(user._id, updatedUserData);
       console.log('User updated:', updatedUser);
-      const updatedUserInLocalStorage = { ...user, ...updatedUserData };
-      localStorage.setItem(CURRENT_USER, JSON.stringify(updatedUserInLocalStorage));
+      // const updatedUserInLocalStorage = { ...user, ...updatedUserData };
+      // localStorage.setItem(CURRENT_USER, JSON.stringify(updatedUserInLocalStorage));
 
       setIsEditing(false);
     } catch (err) {
@@ -87,36 +91,43 @@ export default function MyInfo() {
   };
 
   return (
-    <div>
+    <div >
       {isEditing ? (
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="w-full max-w-md space-y-8">
+        <div className="flex  py-12 px-4 sm:px-6 lg:px-8">
+          <div className=" container">
             <form
               onSubmit={handleSubmit(handleFormSubmit)}
               className="col-md-6 p-3 shadow mx-auto"
             // action="#"
             // method="POST"
+            style={{ border:"solid 2px #54E8A9"}}
             >
-              <div className="-space-y-px rounded-md shadow-sm p-3 text-white">
+ 
+              <div className=" shadow-sm pt-5 text-white text-start" style={{ width: '65%',  margin: '0 auto' }}>
+ 
+                
                 <InputFirstName
                   label="First Name"
                   register={register}
                   errors={errors}
-                  className="relative m-2 block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                  // className="relative m-2 block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                  className='form-control'
                 />
 
                 <InputLastName
                   label="Last Name"
                   register={register}
                   errors={errors}
-                  className="relative m-2 block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                  // className="relative m-2 block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                  className='form-control'
                 />
 
                 <InputEmail
                   label="Email"
                   register={register}
                   errors={errors}
-                  className="relative m-2 block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                  className="relative m-2 block w-full appearance-none  border border-gray-300 px-3 py-2  focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                // style={{ width: '65%', margin: '0 auto' }}
                 />
 
 
@@ -125,30 +136,35 @@ export default function MyInfo() {
                   register={register}
                   errors={errors}
                   name="gender"
-                  className="relative m-2 block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white text-dark"
+                  className="relative m-2 block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white mb-3 text-dark"
                 />
 
-                <AddressInput
-                  label="Address"
-                  onAddressChange={handleAddressChange}
-                  register={register}
-                  errors={errors}
-
-                  defaultValue={user.address}
-                />
               </div>
+              <p className='text-white mt-3 mb-1'>Address</p>
+              <AddressInput
+                label="Address"
+                onAddressChange={handleAddressChange}
+                register={register}
+                errors={errors}
 
-              <div className="flex justify-between">
+                defaultValue={user.address}
+              />
+
+
+              <div className="flex justify-between m-4">
                 <button
                   type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  className="btn button"
+                  style={{width:"80px", marginRight:"16px"}}
                 >
                   Save
                 </button>
                 <button
                   type="button"
                   onClick={handleCancelEdit}
-                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                  className="btn button"
+                  style={{width:"80px" }}
+
                 >
                   Cancel
                 </button>
@@ -158,107 +174,104 @@ export default function MyInfo() {
         </div>
       ) : (
         <section className="container-fluid">
-          <div className="container py-5 h-100 row ">
-            <div className=" d-flex justify-content-center align-items-center h-100 row">
 
 
-              <div className="card col-12">
-                <div
-                  className=" mt-5 rounded-top text-white d-flex flex-row "
-                  style={{ backgroundColor: '#000', height: '200px', width: '900px' }}
+          <div className="card pt-5">
+            <div
+              className=" mt-5 rounded-top text-white d-flex flex-row "
+              style={{ backgroundColor: '#000', height: '150px', borderBottom: '8px solid #54E8A9' }}
+            >
+              <div className="ms-4  d-flex flex-column" style={{ width: '150px',marginTop:"56px" }}>
+                <img
+                  src={user.gender == "male" ? `${imageMale}` : `${imageFemale}`}
+
+                  alt="Generic placeholder image"
+                  className="img-fluid  mt-3 mb-2"
+                  style={{ height: '250px',  zIndex: '1', }}
+                />
+                <button
+                  type="button"
+                  className="btn btn-dark "
+                  data-mdb-ripple-color="dark"
+                  style={{ zIndex: '1' }}
+                  onClick={handleEditProfile}
                 >
-                  <div className="ms-4 mt-4 d-flex flex-column" style={{ width: '150px' }}>
-                    <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                      alt="Generic placeholder image"
-                      className="img-fluid img-thumbnail mt-4 mb-2"
-                      style={{ width: '150px', zIndex: '1' }}
-                    />
-                    <button
-                      type="button"
-                      className="btn btn-dark "
-                      data-mdb-ripple-color="dark"
-                      style={{ zIndex: '1' }}
-                      onClick={handleEditProfile}
-                    >
-                      Edit profile
-                    </button>
-                  </div>
-                  <div className="ms-3" style={{ marginTop: '110px' }}>
-                    <h5>
-                      {user.fullName.firstName} {user.fullName.lastName}
-                    </h5>
-                    <p className="text-danger fw-bold">Registered User</p>
-                  </div>
+                  Edit profile
+                </button>
+              </div>
+              <div className="ms-3" style={{ marginTop: '70px' }}>
+                <h5>
+                  {user.fullName.firstName} {user.fullName.lastName}
+                </h5>
+                <p className=" fw-bold" style={{ color: '#54E8A9' }}>Registered User</p>
+              </div>
+            </div>
+            <div className="p-4" style={{ backgroundColor: '#f8f9fa', color: '#54E8A9' }}>
+              <div className="d-flex justify-content-end text-center py-1">
+                <div>
+                  <p className="mb-1 h5">10</p>
+                  <p className="small  mb-0">Rides</p>
                 </div>
-                <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa', width: '900px' }}>
-                  <div className="d-flex justify-content-end text-center py-1">
-                    <div>
-                      <p className="mb-1 h5">253</p>
-                      <p className="small text-muted mb-0">Photos</p>
-                    </div>
-                    <div className="px-3">
-                      <p className="mb-1 h5">1026</p>
-                      <p className="small text-muted mb-0">Followers</p>
-                    </div>
-                    <div>
-                      <p className="mb-1 h5">478</p>
-                      <p className="small text-muted mb-0">Following</p>
-                    </div>
-                  </div>
+                <div className="px-3">
+                  <p className="mb-1 h5">50</p>
+                  <p className="small  mb-0">Offers</p>
                 </div>
-                <div className="card-body p-0 text-black " style={{ width: "900px" }}>
-                  <div className="">
-                    <p className="lead fw-normal text-white">About</p>
-                    <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
-                      <p className="font-italic mb-1">Registered User</p>
-                      <p className="font-italic mb-1"> {user.gender}</p>
-                      <p className="font-italic mb-1">Lives in {user.address}</p>
-                      <p className="font-italic mb-0">{user.email}</p>
-                      <p className="font-italic mb-0">password: {user.password}</p>
-                    </div>
-                  </div>
-                  <div className="d-flex justify-content-between align-items-center mb-4">
-                    <p className="lead fw-normal mb-0 text-white">Recent photos</p>
-                    <p className="mb-0">
-                      <a href="#!" className="text-muted">
-                        Show all
-                      </a>
-                    </p>
-                  </div>
-                  <div className="row g-2">
-                    <div className="col mb-2">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
-                        alt="image 1"
-                        className="w-100 rounded-3"
-                      />
-                    </div>
-                    <div className="col mb-2">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
-                        alt="image 1"
-                        className="w-100 rounded-3"
-                      />
-                    </div>
-                  </div>
-                  <div className="row g-2">
-                    <div className="col">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
-                        alt="image 1"
-                        className="w-100 rounded-3"
-                      />
-                    </div>
-                    <div className="col">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
-                        alt="image 1"
-                        className="w-100 rounded-3"
-                      />
-                    </div>
-                  </div>
+                <div>
+                  <p className="mb-1 h5">5</p>
+                  <p className="small  mb-0">Requests</p>
                 </div>
+              </div>
+            </div>
+            <div className="card-body p-0 text-black ">
+              <div className="text-start">
+                <p className="lead fw-normal text-white ps-4 pt-3">About</p>
+                <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
+                  <p className="font-italic mb-1">Registered User</p>
+                  <p className="font-italic mb-1"> {user.gender}</p>
+                  <p className="font-italic mb-1">Lives in {user.address}</p>
+                  <p className="font-italic mb-0">{user.email}</p>
+                </div>
+              </div>
+              <div className="d-flex justify-content-between align-items-center mb-4">
+                <p className="lead fw-normal mb-0 text-white pt-3">Recent Rides</p>
+                <p className="mb-0">
+                  <a href="#!" className="text-muted">
+                    Show all
+                  </a>
+                </p>
+              </div>
+              <div className="row g-2">
+                <div className="col mb-2">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
+                    alt="image 1"
+                    className="w-100 rounded-3"
+                  />
+                </div>
+                <div className="col mb-2">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
+                    alt="image 1"
+                    className="w-100 rounded-3"
+                  />
+                </div>
+              </div>
+              <div className="row g-2">
+                <div className="col">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
+                    alt="image 1"
+                    className="w-100 rounded-3"
+                  />
+                </div>
+                <div className="col">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
+                    alt="image 1"
+                    className="w-100 rounded-3"
+                  />
+                </div>
+
               </div>
             </div>
 
--- src/components/inputs/inputEmail.jsx
+++ src/components/inputs/inputEmail.jsx
@@ -17,7 +17,7 @@ const InputEmail = (props) => {
     return (
         <>
             <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="email" className="block text-sm font-medium ">
                    {label}
                 </label>
                 <input   defaultValue={defaultValue}
@@ -26,12 +26,12 @@ const InputEmail = (props) => {
                     name="email"
                     type="text"
                     autoComplete="email"
-                    className={classNames(errors.email ? "relative block w-full appearance-none rounded-t-md  border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    className={classNames(errors.email ? "relative block w-full appearance-none rounded  border border-gray-300 px-3 py-2 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white"
                         :
-                        "relative block w-full appearance-none  rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm")}
+                        "relative block w-full appearance-none  rounded border border-gray-300 px-3 py-2   focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm bg-white")}
                         
                 />
-                {errors.email && <p className='text-white font-bold bg-red-800 text-center rounded-b-md  border-gray-300  py-1'>Enter valid email</p>}
+                {errors.email && <p className='text-white font-bold  text-center rounded  border-gray-300  py-1'>Enter valid email</p>}
             </div>
 
 
